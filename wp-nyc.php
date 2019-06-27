a59dfdd2e10cba389649c519da8207aae1bdb7e0
K. Adam White <kadamwhite@users.noreply.github.com>
2019-06-27 17:35:08 -0400

    Add devserver

--- wp-nyc.php
+++ wp-nyc.php
@@ -10,20 +10,26 @@ Text Domain: wpnyc
 
 namespace WP_NYC_Webpack_Toolkit_Demo;
 
+use Asset_Loader;
+
 /**
  * Enqueue editor assets based on the generated `asset-manifest.json` file.
  *
  * @return void
  */
 function enqueue_block_assets() {
-	wp_enqueue_script(
-		'wp-nyc-editor-blocks',
-		plugin_dir_url( __FILE__ ) . 'build/editor.js',
+	Asset_Loader\autoenqueue(
+		plugin_dir_path( __FILE__ ) . 'build/asset-manifest.json',
+		// Expect the bundle to be generated as editor.js
+		'editor.js',
 		[
-			'wp-blocks',
-			'wp-components',
-			'wp-data',
-			'wp-element',
+			'handle'  => 'wp-nyc-editor-blocks',
+			'scripts' => [
+				'wp-blocks',
+				'wp-components',
+				'wp-data',
+				'wp-element',
+			],
 		]
 	);
 }
@@ -31,10 +37,14 @@ add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_block_asse
 
 
 function enqueue_frontend_assets() {
-	wp_enqueue_script(
-		'wp-nyc-frontend',
-		plugin_dir_url( __FILE__ ) . 'build/frontend.js',
-		[]
+	Asset_Loader\autoenqueue(
+		plugin_dir_path( __FILE__ ) . 'build/asset-manifest.json',
+		// Expect the bundle to be generated as frontend.js
+		'frontend.js',
+		[
+			'handle'  => 'wp-nyc-editor-frontend-scripts',
+			'scripts' => [],
+		]
 	);
 }
 add_action( 'enqueue_block_assets', __NAMESPACE__ . '\\enqueue_frontend_assets' );
