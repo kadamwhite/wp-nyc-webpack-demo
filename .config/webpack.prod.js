b2b69cc6984f74947d342223db12c9b94e63c440
K. Adam White <kadamwhite@users.noreply.github.com>
2019-06-27 17:29:07 -0400

    Add babel and implement a simple block

--- .config/webpack.prod.js
+++ .config/webpack.prod.js
@@ -1,11 +1,12 @@
 // Example webpack.config.prod.js file.
-const { helpers, presets } = require( '@humanmade/webpack-helpers' );
+const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
 const { filePath } = helpers;
 
 module.exports = presets.production( {
+	externals,
 	entry: {
-		editor: filePath( 'src/editor.js' ),
 		frontend: filePath( 'src/frontend.js' ),
+		editor: filePath( 'src/editor.js' ),
 	},
 	output: {
 		path: filePath( 'build' ),
--- src/editor.js
+++ src/editor.js
@@ -1,1 +1,24 @@
-console.log( 'I am running in the editor' );
+import { registerBlockType } from '@wordpress/blocks';
+import { Fragment } from '@wordpress/element';
+
+registerBlockType( 'wpnyc/demo-1', {
+	title: 'Demo Block',
+	description: 'A simple demo block',
+	category: 'widgets',
+	icon: 'star-filled',
+	edit( { isSelected } ) {
+		return (
+			<Fragment>
+				<p>Demo Editor Content</p>
+				{ isSelected && (
+					<p><em>(selected, woo!)</em></p>
+				) }
+			</Fragment>
+		);
+	},
+	save() {
+		return (
+			<p>Demo Saved Content</p>
+		);
+	},
+} );
